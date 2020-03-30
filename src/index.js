5eecfb6958499005b574a6cb2d0e6a1db1799403
prakhar308 <khandelwalprakhar30@gmail.com>
2020-03-30 14:39:38 +0530

    remove app.js and add recipe and navbar components

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import RecipeApp from './RecipeApp';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <RecipeApp />
   </React.StrictMode>,
   document.getElementById('root')
 );
